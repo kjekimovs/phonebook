b9655ee518213fc9e7fa2bbbcc2d6c6c33fa55c1
Kos <kjekimovs@evolutiongaming.com>
2017-03-29 19:06:58 +0300

    3

--- src/main/java/javaschool/app/PhoneBook.java
+++ src/main/java/javaschool/app/PhoneBook.java
@@ -13,30 +13,17 @@ public class PhoneBook implements ShellDependent {
     private List<Record> recordList = new ArrayList<>();
 
     @Command
-    public void create(String name, String email, String... phones) {
-        Record r = new Record();
+    public void createPerson(String name, String email, String... phones) {
+        Person r = new Person();
         r.setName(name);
         r.setEmail(email);
         r.addPhones(phones);
-
         recordList.add(r);
     }
 
     @Command
     public List<Record> list() {
         return recordList;
-    }
-
-
-    @Command
-    public void edit(int id) throws IOException {
-        for (Record r : recordList) {
-            if (r.getId() == id) {
-                ShellFactory.createSubshell("#" + id, theShell, "Edit record", r)
-                        .commandLoop();
-                break;
-            }
-        }
     }
 
     @Command
@@ -50,17 +37,35 @@ public class PhoneBook implements ShellDependent {
     }
 
     @Command
+    public void edit(int id) throws IOException {
+        for (Record r : recordList) {
+            if (r.getId() == id) {
+                ShellFactory.createSubshell("#" + id, theShell, "Edit record", r)
+                        .commandLoop();
+                break;
+            }
+        }
+    }
+
+    @Command
     public List<Record> find(String str) {
         str = str.toLowerCase();
         List<Record> result = new ArrayList<>();
         for (Record r : recordList) {
             String name = r.getName().toLowerCase();
-            if (name.contains(str)) {
+            String email = r.getEmail().toLowerCase();
+            if (name.contains(str) || email.contains(str)) {
                 result.add(r);
+            } else {
+                for (String p : r.getPhones()) {
+                    p = p.toLowerCase();
+                    if (p.contains(str)) {
+                        result.add(r);
+                        break;
+                    }
+                }
             }
-
         }
-
         return result;
     }
 
